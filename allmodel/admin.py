3d61267f8006c72861f3e128f028d55e6a52929c
rahul292-cloud <imrsy292@gmail.com>
2020-04-03 13:56:54 +0530

    [chg in admin]

--- allmodel/admin.py
+++ allmodel/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .client import Client
+from .company import Company
+from .product import Product
+from .purchase import Purchase
 
+admin.site.register(Client)
+admin.site.register(Company)
+admin.site.register(Purchase)
 # Register your models here.
