15cb3197864805b2a96ec869ea2ba12882c95285
rahul292-cloud <imrsy292@gmail.com>
2020-04-27 13:52:49 +0530

    [reset password added]

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
 
@@ -8,6 +10,24 @@ urlpatterns = [
     path('login',views.LoginPage.as_view(), {'login': ''}, name='login'),
     path('logout',views.logoutUser, name='logout' ),
     path('user/', views.userPage, name="user-page"),
+
+    path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="dashboard/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/',
+        auth_views.PasswordResetDoneView.as_view(template_name="dashboard/password_reset_sent.html"),
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="dashboard/password_reset_form.html"),
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name="dashboard/password_reset_done.html"),
+        name="password_reset_complete"),
+
+
 
     path('account/', views.accountSettings, name="account"),
 
