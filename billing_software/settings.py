c3fa20395fc91f0edb5f42953109b66226e6601f
rahul292-cloud <imrsy292@gmail.com>
2020-04-27 13:56:45 +0530

    chg in setting for reset pass

--- billing_software/settings.py
+++ billing_software/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'imrsy292@gmail.com'
-EMAIL_HOST_PASSWORD = 'rH@123456'
+EMAIL_HOST_PASSWORD = '*********'
